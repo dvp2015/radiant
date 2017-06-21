5bf079aa5a6bf7672a91904a188da18bae00e064
Korijn van Golen <k.vangolen@clinicalgraphics.com>
2017-06-21 21:38:49 +0200

    More scaffolding, imports, stub test

--- radiant/__init__.py
+++ radiant/__init__.py
@@ -1,3 +1,7 @@
 """Python 3D rendering library."""
+from .geometries import *
+from .materials import *
+from .objects import *
+from .renderers import *
 
 __version__ = '0.1.0'
--- radiant/objects.py
+++ radiant/objects.py
@@ -1,7 +1,9 @@
 class Object3D:
-    pass
+    def __init__(self):
+        self.world = [[1, 0, 0, 0], [0, 1, 0, 0], [0, 0, 1, 0], [0, 0, 0, 1]]
 
 class Mesh(Object3D):
     def __init__(self, geometry, material):
+        super().__init__()
         self.geometry = geometry
         self.material = material
