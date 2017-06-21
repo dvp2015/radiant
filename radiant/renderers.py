fef0cb1c2ead7fe4e553944ebd7ca9a3f2e69e10
Korijn van Golen <k.vangolen@clinicalgraphics.com>
2017-06-21 21:40:58 +0200

    added fbo target to render signature

--- radiant/renderers.py
+++ radiant/renderers.py
@@ -5,5 +5,5 @@ class ModernGLRenderer:
     def __init__(self, context):
         self.context = context
     
-    def render(self, scene, camera):
+    def render(self, scene, camera, fbo=None):
         pass
