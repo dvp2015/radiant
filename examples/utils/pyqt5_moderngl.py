ba6d34dbdc240f52511ef9412bd8a274329dff45
Korijn van Golen <k.vangolen@clinicalgraphics.com>
2017-09-15 15:57:34 +0200

    Parsing Qt mouse and key events. Render loop through timer.

--- examples/utils/pyqt5_moderngl.py
+++ examples/utils/pyqt5_moderngl.py
@@ -1,9 +1,9 @@
 import signal
 import sys
 
-from PyQt5.QtCore import Qt
-from PyQt5.QtGui import QSurfaceFormat, QCursor
-from PyQt5.QtWidgets import QApplication, QWidget, QOpenGLWidget, QHBoxLayout
+from PyQt5.QtCore import QEvent, Qt, QTimer
+from PyQt5.QtGui import QCursor, QSurfaceFormat, QWheelEvent
+from PyQt5.QtWidgets import QApplication, QHBoxLayout, QOpenGLWidget, QWidget
 
 from radiant.renderers.moderngl import ModernGLRenderer
 
@@ -30,9 +30,67 @@ class Window(QWidget):
         QApplication.setOverrideCursor(QCursor(Qt.BlankCursor))
 
 
+def mouse_event(e):
+    """Map a PyQt5 mouse event to a radiant event."""
+    type_map = {
+        QEvent.MouseButtonPress: "Press",
+        QEvent.MouseButtonRelease: "Release",
+        QEvent.MouseButtonDblClick: "DblClick",
+        QEvent.MouseMove: "Move",
+    }
+
+    button_map = {
+        "Left": Qt.LeftButton,
+        "Right": Qt.RightButton,
+        "Middle": Qt.MiddleButton,
+    }
+
+    modifier_map = {
+        "Shift": Qt.ShiftModifier,
+        "Ctrl": Qt.ControlModifier,
+        "Alt": Qt.AltModifier,
+    }
+
+    re = {
+        'pos': (e.x(), e.y()),
+        'buttons': [button for button, qt_button in button_map.items() if e.buttons() & qt_button],
+        'modifiers': [key for key, qt_modifier in modifier_map.items() if e.modifiers() & qt_modifier],
+    }
+
+    if isinstance(e, QWheelEvent):
+        re['type'] = "Wheel"
+        re['delta'] = (e.angleDelta().x(), e.angleDelta().y())
+    else:
+        re['type'] = type_map[e.type()]
+
+    return re
+
+
+def key_event(e):
+    """Map a PyQt5 key event to a radiant event."""
+    type_map = {
+        QEvent.KeyPress: "Press",
+        QEvent.KeyRelease: "Release",
+    }
+
+    modifier_map = {
+        "Shift": Qt.ShiftModifier,
+        "Ctrl": Qt.ControlModifier,
+        "Alt": Qt.AltModifier,
+    }
+
+    return {
+        'type': type_map[e.type()],
+        'key': e.text(),
+        'modifiers': [key for key, qt_modifier in modifier_map.items() if e.modifiers() & qt_modifier],
+    }
+
+
 class GLWidget(QOpenGLWidget):
     def __init__(self, parent=None):
         super().__init__(parent)
+
+        self.setFocusPolicy(Qt.StrongFocus)
 
         fmt = QSurfaceFormat()
         fmt.setVersion(3, 3)
@@ -42,6 +100,10 @@ class GLWidget(QOpenGLWidget):
 
         self.setMouseTracking(True)
 
+        self.render_loop = QTimer()
+        self.render_loop.timeout.connect(self.update)
+        self.render_loop.start(1000/30)  # 30 FPS!
+
     def initializeGL(self):
         self.renderer = ModernGLRenderer()
 
@@ -49,19 +111,26 @@ class GLWidget(QOpenGLWidget):
         self.renderer.viewport = (0, 0, width, height)
 
     def paintGL(self):
+        print("rendering")
         self.renderer.render(self.scene, self.camera, self.light)
 
     def keyPressEvent(self, e):
-        if e.key() == Qt.Key_Escape:
-            self.close()
+        print(key_event(e))
+
+    def keyReleaseEvent(self, e):
+        print(key_event(e))
+
+    def mousePressEvent(self, e):
+        print(mouse_event(e))
+
+    def mouseReleaseEvent(self, e):
+        print(mouse_event(e))
 
     def mouseMoveEvent(self, e):
-        pass
+        print(mouse_event(e))
 
-    def leaveEvent(self, e):
-        size = self.geometry()
-        pos = QCursor.pos()
-        pass
+    def wheelEvent(self, e):
+        print(mouse_event(e))
 
 
 def show_scene(title, scene, camera, light):
