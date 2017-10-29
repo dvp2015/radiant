dfff37e4e8b9e39bad71f02ff477a4ed69e7ec1e
Korijn van Golen <k.vangolen@clinicalgraphics.com>
2017-10-29 10:37:54 +0100

    packaging stuff

--- setup.py
+++ setup.py
@@ -1,10 +1,25 @@
-from setuptools import find_packages, setup
+import os
+import re
+
+from setuptools import setup, find_packages
+
+
+with open(os.path.join("radiant", "__init__.py"), mode="r") as fh:
+    version = re.match("__version__\s*=\s*['\"](.*?)['\"]", fh.read()).group(1)
+packages = find_packages()
+with open("requirements.txt", mode="r") as fh:
+    install_requires = fh.readlines()
 
 
 setup(
     name='radiant',
-    version="0.0.1",
-    packages=find_packages(),
+    version=version,
+    packages=packages,
+    install_requires=install_requires,
+    extras_require={
+        'PyQt5': ["PyQt5"],
+        'moderngl': ["moderngl"],
+    },
     description="",
     long_description="",
     author="Korijn van Golen",
